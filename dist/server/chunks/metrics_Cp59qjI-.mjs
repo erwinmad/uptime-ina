3730f08f68de89d7575ae634af927920b5b0326c
erwinmad <erwinmad@users.noreply.github.com>
2026-09-05 12:44:29 +0700

    Add .gitignore and remove untracked artifacts

--- dist/server/chunks/metrics_Cp59qjI-.mjs
+++
@@ -1,5 +0,0 @@
-import { n as metrics_exports } from "./metrics_Btw52ztG.mjs";
-//#region \0virtual:astro:page:src/pages/api/metrics@_@ts
-var page = () => metrics_exports;
-//#endregion
-export { page };
